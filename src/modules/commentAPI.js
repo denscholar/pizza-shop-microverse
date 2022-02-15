14096393af3741a34135cec0634f01da3259590b
Boaz Aduda <60380365+Aduda-Boaz@users.noreply.github.com>
2022-02-15 23:22:04 +0300

    Edited the involvement API ID

--- src/modules/commentAPI.js
+++ src/modules/commentAPI.js
@@ -1,11 +1,12 @@
 const getComments = async (id) => {
-  const json = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lyq9jUNEHrKhgXNSU82r/comments?item_id=${id}`)
+  const json = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/GpS015MsEJIxdqPoPigd
+/comments?item_id=${id}`)
     .then((response) => response.json());
   return json;
 };
 
 const addComments = async (id, username, comments) => {
-  const commentInfo = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lyq9jUNEHrKhgXNSU82r/comments', {
+  const commentInfo = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/GpS015MsEJIxdqPoPigd/comments', {
     method: 'POST',
     body: JSON.stringify({
       item_id: id,
